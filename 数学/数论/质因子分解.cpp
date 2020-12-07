6ee393202555f4cdfe06f7a214c9ac0ce51dcce5
linbinwu <linbinwu1999@gmail.com>
2020-12-07 17:50:37 +0800

    new update

--- 数学/数论/质因子分解.cpp
+++ 数学/数论/质因子分解.cpp
@@ -1,13 +1,21 @@
-vector<pii> store;
+int vis[N], prime[N], num;
+void makePrime(int siz) {
+    num = 0;
+    for (int i = 2; i <= siz; i++) {
+        if (!vis[i]) prime[++num] = i;
+        for (int j = 1; j <= num && i * prime[j] <= siz; j++) {
+            vis[i * prime[j]] = 1;
+            if (i % prime[j] == 0) break;
+        }
+    }
+}
 
 void divide(ll x) {
-    for (ll i = 2; i * i <= x; i++)
-        if (x % i == 0) {
+    for (int i = 1; i <= num && 1ll * prime[i] * prime[i] <= x; i++)
+        if (x % prime[i] == 0) {
             int cnt = 0;
-            while (x % i == 0) {
-                x /= i, cnt++;
-            }
-            store.push_back(make_pair(i, cnt));
+            while (x % prime[i] == 0) x /= prime[i], cnt++;
+            store.push_back({prime[i], cnt});
         }
-    if (x > 1) store.push_back(make_pair(x, 1));
+    if (x > 1) store.push_back({x, 1});
 }
